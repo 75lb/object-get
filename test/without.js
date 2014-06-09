398f2be3af8e380b87bb9e8714e3c54ea58d0bef
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-09 16:29:34 +0100

    lint, test fix

--- test/without.js
+++ test/without.js
@@ -7,16 +7,10 @@ test("without (object)", function(t){
 		two: 2,
 		three: 3,
 		four: 4
-    }
+    };
     t.deepEqual(w.without(object, [ "two", "three" ]), {
     	one: 1,
 		four: 4
     });
-    t.end();
-});
-
-test("without (array)", function(t){
-    var array = [ 1,2,3,4 ];
-    t.deepEqual(w.without(array, [ 2, 3 ]), [ 1, 4 ]);
     t.end();
 });
